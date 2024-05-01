88e2efc4b7b3d5a19292ab27dab6e254665f7313
Sarvjeet Singh <sarvjeet@gmail.com>
2024-04-30 20:25:29 -0700

    Bumped version to 1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='ibonds',
-    version='1.0.3',
+    version='1.0.4',
     author='Sarvjeet Singh',
     author_email='sarvjeet@gmail.com',
     description=('Library to calculate the current value of a '
